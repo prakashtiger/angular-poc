41a50a83f154fe0e4a7570fd70044c740260f5a2
prakashtiger <akashtitans@gmail.com>
2019-11-24 17:03:43 +0530

    twitter component update

--- src/app/twitter-stream/twitter-stream.component.ts
+++ src/app/twitter-stream/twitter-stream.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as TwitterStreamActions from '../twitter-stream.action';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-twitter-stream',
   templateUrl: './twitter-stream.component.html',
   styleUrls: ['./twitter-stream.component.css']
 })
 export class TwitterStreamComponent implements OnInit {
-
-  constructor() { }
+  twitterStreamList$: Observable<any[]> = this.store.select((state) => {
+    return state.twitterStreamList.twitterStreamList;
+  });
+ 
+  constructor(private store: Store<any>) { 
+    
+  }
 
   ngOnInit() {
+    this.store.dispatch(new TwitterStreamActions.List());
+  }
+
+  filterTweet() {
+    
   }
 
 }
